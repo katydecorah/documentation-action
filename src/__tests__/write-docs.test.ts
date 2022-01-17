3d86615707b92317459a274daaf00a05e2670e8f
Katy DeCorah <katydecorah@gmail.com>
2022-01-17 10:59:46 -0500

    Add test, test coverage, ci

--- src/__tests__/write-docs.test.ts
+++ src/__tests__/write-docs.test.ts
@@ -1,7 +1,9 @@
 import { writeDocs } from "../write-docs";
 import fs from "fs";
+import { exportVariable } from "@actions/core";
 
 jest.mock("fs");
+jest.mock("@actions/core");
 
 const exampleReadMe = `# documentation-action
 
@@ -45,6 +47,7 @@ Hello!
       <!-- END GENERATED DOCUMENTATION -->",
       ]
     `);
+    expect(exportVariable).toHaveBeenCalledWith("UpdateDocumentation", true);
   });
 
   test("without comments", async () => {
@@ -56,7 +59,7 @@ Hello!
         "./README.md",
         "# documentation-action
 
-      A GitHub action to help document your GitHub actions. 
+      A GitHub action to help document your GitHub actions.
 
       <!-- START GENERATED DOCUMENTATION -->
 
@@ -66,9 +69,24 @@ Hello!
       name: Document Github action
       on: release
       \`\`\`
-      <!-- END GENERATED DOCUMENTATION -->",
+      <!-- END GENERATED DOCUMENTATION -->
+      ",
       ]
     `);
+    expect(exportVariable).toHaveBeenCalledWith("UpdateDocumentation", true);
+  });
+
+  test("no changes", async () => {
+    jest.spyOn(fs, "readFileSync").mockImplementationOnce(
+      () => `# documentation-action
+A GitHub action to help document your GitHub actions.
+<!-- START GENERATED DOCUMENTATION -->
+## Set up the workflow<!-- END GENERATED DOCUMENTATION -->`
+    );
+    const writeMock = jest.spyOn(fs, "writeFileSync");
+    writeDocs(`## Set up the workflow`, "README.md");
+    expect(writeMock).not.toHaveBeenCalled();
+    expect(exportVariable).toHaveBeenCalledWith("UpdateDocumentation", false);
   });
 
   test("can't find file", async () => {
