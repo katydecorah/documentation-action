8470c517bc31fabf2ddfbfd70d07af987ca3c13e
Katy DeCorah <katydecorah@gmail.com>
2022-09-22 08:21:32 -0400

    Clean up

--- src/__tests__/build-docs.test.ts
+++ src/__tests__/build-docs.test.ts
@@ -39,15 +39,125 @@ describe("buildDocs", () => {
             - "action.yml"
             - "package.json"
             - "README.md"
-        workflow_dispatch:
-          inputs:
-            bookIsbn:
-              description: The book's ISBN
-              required: true
-              type: string
-            notes:
-              description: Notes about the book
-              type: string
+
+      jobs:
+        document_action:
+          runs-on: macOS-latest
+          name: Write documentation
+          steps:
+            - name: Checkout
+              uses: actions/checkout@v3
+            - name: Documentation action
+              uses: katydecorah/documentation-action@v0.1.0
+            - name: Commit files
+              if: env.UpdateDocumentation == 'true'
+              run: |
+                git config --local user.email "action@github.com"
+                git config --local user.name "GitHub Action"
+                git commit -am "Update documentation"
+                git push
+      \`\`\`
+
+      ## Action options
+
+      - \`exampleWorkflowFile\`: The example workflow file in \`.github/workflows/\` Default: \`example.yml\`.
+
+      - \`documentationFile\`: The file where the action will write and update documentation for the action. Default: \`README.md\`.
+
+      "
+    `);
+  });
+  test("without inputs", () => {
+    expect(
+      buildDocs({
+        workflow,
+        release: "katydecorah/documentation-action@v0.1.0",
+        action: {},
+      })
+    ).toMatchInlineSnapshot(`
+      "
+      ## Set up the workflow
+
+      To use this action, create a new workflow in \`.github/workflows\` and modify it as needed:
+
+      \`\`\`yml
+      name: Document GitHub action
+
+      on:
+        push:
+          paths:
+            - ".github/workflows/example.yml"
+            - "action.yml"
+            - "package.json"
+            - "README.md"
+
+      jobs:
+        document_action:
+          runs-on: macOS-latest
+          name: Write documentation
+          steps:
+            - name: Checkout
+              uses: actions/checkout@v3
+            - name: Documentation action
+              uses: katydecorah/documentation-action@v0.1.0
+            - name: Commit files
+              if: env.UpdateDocumentation == 'true'
+              run: |
+                git config --local user.email "action@github.com"
+                git config --local user.name "GitHub Action"
+                git commit -am "Update documentation"
+                git push
+      \`\`\`"
+    `);
+  });
+
+  test("with workflow inputs", () => {
+    expect(
+      buildDocs({
+        workflow: {
+          ...workflow,
+          json: {
+            ...workflow.json,
+            on: {
+              ...workflow.json.on,
+
+              workflow_dispatch: {
+                inputs: {
+                  bookIsbn: {
+                    description: "The book's ISBN",
+                    required: true,
+                    type: "string",
+                  },
+
+                  notes: {
+                    description: "Notes about the book",
+                    type: "string",
+                  },
+                },
+              },
+            },
+          },
+        },
+
+        release: "katydecorah/documentation-action@v0.1.0",
+        action,
+      })
+    ).toMatchInlineSnapshot(`
+      "
+      ## Set up the workflow
+
+      To use this action, create a new workflow in \`.github/workflows\` and modify it as needed:
+
+      \`\`\`yml
+      name: Document GitHub action
+
+      on:
+        push:
+          paths:
+            - ".github/workflows/example.yml"
+            - "action.yml"
+            - "package.json"
+            - "README.md"
 
       jobs:
         document_action:
@@ -89,142 +199,6 @@ describe("buildDocs", () => {
       "
     `);
   });
-  test("without inputs", () => {
-    expect(
-      buildDocs({
-        workflow,
-        release: "katydecorah/documentation-action@v0.1.0",
-        action: {},
-      })
-    ).toMatchInlineSnapshot(`
-      "
-      ## Set up the workflow
-
-      To use this action, create a new workflow in \`.github/workflows\` and modify it as needed:
-
-      \`\`\`yml
-      name: Document GitHub action
-
-      on:
-        push:
-          paths:
-            - ".github/workflows/example.yml"
-            - "action.yml"
-            - "package.json"
-            - "README.md"
-        workflow_dispatch:
-          inputs:
-            bookIsbn:
-              description: The book's ISBN
-              required: true
-              type: string
-            notes:
-              description: Notes about the book
-              type: string
-
-      jobs:
-        document_action:
-          runs-on: macOS-latest
-          name: Write documentation
-          steps:
-            - name: Checkout
-              uses: actions/checkout@v3
-            - name: Documentation action
-              uses: katydecorah/documentation-action@v0.1.0
-            - name: Commit files
-              if: env.UpdateDocumentation == 'true'
-              run: |
-                git config --local user.email "action@github.com"
-                git config --local user.name "GitHub Action"
-                git commit -am "Update documentation"
-                git push
-      \`\`\`## Trigger the action
-
-      To trigger the action, [create a workflow dispatch event](https://docs.github.com/en/rest/actions/workflows#create-a-workflow-dispatch-event) with the following body parameters:
-
-      \`\`\`js
-      { 
-        "ref": "main", // Required. The git reference for the workflow, a branch or tag name.
-        "inputs": {
-          "bookIsbn": "", // Required.  The book's ISBN.
-          "notes": "", //  Notes about the book.
-        }
-      }
-      \`\`\`
-      "
-    `);
-  });
-
-  test("without workflow inputs", () => {
-    expect(
-      buildDocs({
-        workflow: {
-          ...workflow,
-          json: {
-            ...workflow.json,
-            on: {
-              ...workflow.json.on,
-              workflow_dispatch: {},
-            },
-          },
-        },
-
-        release: "katydecorah/documentation-action@v0.1.0",
-        action,
-      })
-    ).toMatchInlineSnapshot(`
-      "
-      ## Set up the workflow
-
-      To use this action, create a new workflow in \`.github/workflows\` and modify it as needed:
-
-      \`\`\`yml
-      name: Document GitHub action
-
-      on:
-        push:
-          paths:
-            - ".github/workflows/example.yml"
-            - "action.yml"
-            - "package.json"
-            - "README.md"
-        workflow_dispatch:
-          inputs:
-            bookIsbn:
-              description: The book's ISBN
-              required: true
-              type: string
-            notes:
-              description: Notes about the book
-              type: string
-
-      jobs:
-        document_action:
-          runs-on: macOS-latest
-          name: Write documentation
-          steps:
-            - name: Checkout
-              uses: actions/checkout@v3
-            - name: Documentation action
-              uses: katydecorah/documentation-action@v0.1.0
-            - name: Commit files
-              if: env.UpdateDocumentation == 'true'
-              run: |
-                git config --local user.email "action@github.com"
-                git config --local user.name "GitHub Action"
-                git commit -am "Update documentation"
-                git push
-      \`\`\`
-
-      ## Action options
-
-      - \`exampleWorkflowFile\`: The example workflow file in \`.github/workflows/\` Default: \`example.yml\`.
-
-      - \`documentationFile\`: The file where the action will write and update documentation for the action. Default: \`README.md\`.
-
-      "
-    `);
-  });
 });
 
 test("trimExampleWorkflow", () => {
@@ -243,15 +217,6 @@ test("trimExampleWorkflow", () => {
           - "action.yml"
           - "package.json"
           - "README.md"
-      workflow_dispatch:
-        inputs:
-          bookIsbn:
-            description: The book's ISBN
-            required: true
-            type: string
-          notes:
-            description: Notes about the book
-            type: string
 
     jobs:
       document_action:
