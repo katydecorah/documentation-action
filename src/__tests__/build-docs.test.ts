aabb9b8f482091d47f298db69c8156e9fb29e25e
Katy DeCorah <katydecorah@gmail.com>
2023-02-09 20:09:04 -0500

    Clean up

--- src/__tests__/build-docs.test.ts
+++ src/__tests__/build-docs.test.ts
@@ -45,15 +45,6 @@ describe("buildDocs", () => {
         contents: write
 
       on:
-        workflow_dispatch:
-          inputs:
-            bookIsbn:
-              description: The book's ISBN.
-              required: true
-              type: string
-            notes:
-              description: Notes about the book.
-              type: string
         push:
           paths:
             - ".github/workflows/example.yml"
@@ -88,21 +79,6 @@ describe("buildDocs", () => {
       - \`additional-workflow-file-prefix\`: To include additional workflow files, save them with this prefix in \`.github/workflow/\`. Default: \`example\`.
 
       - \`documentation-file\`: The file where the action will write and update documentation for the action. Default: \`README.md\`.
-
-      ## Trigger the action
-
-      To trigger the action, [create a workflow dispatch event](https://docs.github.com/en/rest/actions/workflows#create-a-workflow-dispatch-event) with the following body parameters:
-
-      \`\`\`js
-      {
-        "ref": "main", // Required. The git reference for the workflow, a branch or tag name.
-        "inputs": {
-          "bookIsbn": "", // Required. The book's ISBN.
-          "notes": "", // Notes about the book.
-        }
-      }
-      \`\`\`
-
       "
     `);
   });
@@ -126,15 +102,6 @@ describe("buildDocs", () => {
         contents: write
 
       on:
-        workflow_dispatch:
-          inputs:
-            bookIsbn:
-              description: The book's ISBN.
-              required: true
-              type: string
-            notes:
-              description: Notes about the book.
-              type: string
         push:
           paths:
             - ".github/workflows/example.yml"
@@ -160,22 +127,7 @@ describe("buildDocs", () => {
                 git config --local user.name "GitHub Action"
                 git commit -am "Update documentation"
                 git push
-      \`\`\`
-      ## Trigger the action
-
-      To trigger the action, [create a workflow dispatch event](https://docs.github.com/en/rest/actions/workflows#create-a-workflow-dispatch-event) with the following body parameters:
-
-      \`\`\`js
-      {
-        "ref": "main", // Required. The git reference for the workflow, a branch or tag name.
-        "inputs": {
-          "bookIsbn": "", // Required. The book's ISBN.
-          "notes": "", // Notes about the book.
-        }
-      }
-      \`\`\`
-
-      "
+      \`\`\`"
     `);
   });
 
@@ -266,15 +218,6 @@ test("trimExampleWorkflow", () => {
       contents: write
 
     on:
-      workflow_dispatch:
-        inputs:
-          bookIsbn:
-            description: The book's ISBN.
-            required: true
-            type: string
-          notes:
-            description: Notes about the book.
-            type: string
       push:
         paths:
           - ".github/workflows/example.yml"
